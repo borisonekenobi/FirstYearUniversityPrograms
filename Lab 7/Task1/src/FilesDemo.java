d3b58b3172c34592ef076c72b810f777198c656c
borisonekenobi <borisonekenobi@gmail.com>
2022-11-18 10:52:49 -0500

    Updated to save file using multiple lines instead of 1 big block of text

--- Lab 7/Task1/src/FilesDemo.java
+++ Lab 7/Task1/src/FilesDemo.java
@@ -1,19 +1,18 @@
-import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class FilesDemo {
-	public static void main (String[] args) throws FileNotFoundException { // main method that can throw a FileNotFoundException
+	public static void main (String[] args) throws IOException { // main method that can throw a FileNotFoundException
 		String fileName = "lines.txt"; // String variable that stores the name of the file
 
 		FileSave fileSave = new FileSave(fileName); // FileSave variable that will create the file
-		fileSave.save("""
-				1-Lorem ipsum dolor sit amet
-				2-Consectetuer adipiscing elit
-				3-Sed diam nonummy nibh euismod tincidunt
-				4-Ut wisi enim ad minim veniam
-				5-Quis nostrud exerci tation ullamcorper
-				6-Suscipit lobortis nisl ut aliquip ex ea commodo consequat
-				7-Duis autem vel eum iriure dolor in hendrerit
-				8-Vel illum dolore eu feugiat nulla facilisis at vero eros"""); // save the block of text to the file
+		fileSave.save("1-Lorem ipsum dolor sit amet"); // save string to the file
+		fileSave.save("2-Consectetuer adipiscing elit"); // save string to the file
+		fileSave.save("3-Sed diam nonummy nibh euismod tincidunt"); // save string to the file
+		fileSave.save("4-Ut wisi enim ad minim veniam"); // save string to the file
+		fileSave.save("5-Quis nostrud exerci tation ullamcorper"); // save string to the file
+		fileSave.save("6-Suscipit lobortis nisl ut aliquip ex ea commodo consequat"); // save string to the file
+		fileSave.save("7-Duis autem vel eum iriure dolor in hendrerit"); // save string to the file
+		fileSave.save("8-Vel illum dolore eu feugiat nulla facilisis at vero eros"); // save string to the file
 
 		FileDisplay fileDisplay = new FileDisplay(fileName); // FileDisplay variable that will display the variable
 		fileDisplay.display(); // display the entire file
