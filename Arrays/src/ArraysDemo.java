13e8d8b126aebb6b555a7a61263a2bdbeeb2ba71
borisonekenobi <borisonekenobi@gmail.com>
2022-11-18 10:53:45 -0500

    Wednesday + Friday lecture

--- Arrays/src/ArraysDemo.java
+++ Arrays/src/ArraysDemo.java
@@ -1,24 +1,44 @@
+import java.util.Scanner;
+
 public class ArraysDemo {
+	public static int[] arrayExpand(int[] oldArray, int newSize) {
+		// create a new array with the larger size equal to newSize
+		int[] newArray = new int[newSize];
+
+		// copy all elements from old array to the new array
+		for (int i = 0; i < oldArray.length; i++) {
+			newArray[i] = oldArray[i];
+		}
+
+		// return the new array
+		return newArray;
+	}
+
 	public static void main (String[] args) {
-//		int lab1 = 10;
-//		int lab2 = 10;
-//		int lab3 = 10;
-//		int lab4 = 8;
-//		int lab5 = 6;
-		int[] lab = new int[8];
-		lab[0] = 10;
-		lab[1] = 10;
-		lab[2] = 10;
-		lab[3] = 8;
-		lab[4] = 6;
-		lab[5] = 10;
-		lab[6] = 10;
-		lab[7] = 10;
+		Scanner keyboard = new Scanner(System.in);
+//		System.out.print("Enter the number of labs: ");
+//		int noOfLabs = keyboard.nextInt();
+//
+//		int[] lab = new int[noOfLabs];
+//
+//		for (int i = 0; i < noOfLabs; i++) {
+//			System.out.println("Enter grade for lab #" + (i + 1) + ": ");
+//			lab[i] = keyboard.nextInt();
+//		}
+
+		int[] lab = {10, 10};
 
 		int total = 0;
+
 		for (int i = 0; i < lab.length; i++) {
 			total += lab[i];
 		}
-		System.out.println("The average is " + total / 8);
+		System.out.println("The average is " + total / lab.length);
+
+		int[] moreLabs = arrayExpand(lab, lab.length + 3);
+
+		for (int i = 0; i < moreLabs.length; i++) {
+			System.out.println(moreLabs[i]);
+		}
 	}
 }
